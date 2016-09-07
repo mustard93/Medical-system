b9a09ee3cfa7c43b05a86cd7c9d22ba8f26b2b09
kaindy7633 <3494688@qq.com>
2016-09-07 14:16:40 +0800

    调整目录结构

--- src/angularjs/modules/main/filters.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by hao on 16/1/7.
- */
-define('main/filters', ['main/init'], function () {
-    angular.module('manageApp.main');
-});
--- src/modules/project/filters.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by hao on 16/1/7.
- */
-define('project/filters', ['project/init'], function () {
-    angular.module('manageApp.project');
-});
