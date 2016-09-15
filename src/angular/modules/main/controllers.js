0c789f7f9c3c156814547ea3ec5abf96b50153ed
kaindy7633 <kaindy7633@gmail.com>
2016-09-16 00:04:08 +0800

    1.调整目录结构 2.修复原框架部分JS代码不正确的警告问题 3.修复合并带来的问题

--- src/angular/modules/main/controllers.js
+++ src/angular/modules/main/controllers.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by hao on 15/11/5.
  */
@@ -24,7 +23,7 @@ define('main/controllers', ['main/init'], function () {
         if ($scope.mainConfig.getMainInfo) {
             var _url=$scope.mainConfig.getMainInfo;
           if(Config.serverPath){
-              if(_url.indexOf("http://")==0||_url.indexOf("https://")==0){
+              if(_url.indexOf("http://")===0||_url.indexOf("https://")===0){
 
               }else {
                   _url=$scope.mainConfig.serverPath+_url;
@@ -64,90 +63,5 @@ define('main/controllers', ['main/init'], function () {
     angular.module('manageApp.main')
         .controller('mainCtrl',  ["$scope",mainCtrl])
         .controller('sideNav',  ["$scope",sideNav])
-        .controller('pageCtrl',  ["$scope","modal",pageCtrl])
+        .controller('pageCtrl',  ["$scope","modal",pageCtrl]);
 });
-=======
-/**
- * Created by hao on 15/11/5.
- */
-
-define('main/controllers', ['main/init'], function () {
-
-    /**
-     * 主控
-     */
-    function mainCtrl($scope) {
-        $scope.mainStatus = {
-            navFold: document.body.clientWidth < 1500,
-            navigation: "",
-            msgBubble: 0 //消息气泡
-        };
-        $scope.mainConfig = window.Config || {};
-        //页面跳转
-        $scope.pageTo = function (_url) {
-            window.location.assign(_url);
-        };
-
-        //获取主要信息
-        if ($scope.mainConfig.getMainInfo) {
-            var _url=$scope.mainConfig.getMainInfo;
-
-            if(Config.serverPath){
-              if(_url.indexOf("http://")!=0&&_url.indexOf("https://")!=0){
-                      _url=$scope.mainConfig.serverPath+_url;
-              }
-            }
-
-            $.ajax({
-                url: _url,
-              type: 'get',
-              xhrFields:{withCredentials: true},
-               crossDomain:true,
-              dataType: 'json',
-              success: function (_data) {
-                if (_data.code == 200) {
-                    angular.extend($scope.mainStatus, _data.data);
-                      $scope.$digest();
-                }
-                 else {
-                  alert(_data.msg || '登录失败');
-                }
-              }
-            });
-
-            $.getJSON(_url, function (_data) {
-                    if (_data.code == 200) {
-                        angular.extend($scope.mainStatus, _data.data);
-                    }
-                })
-                .complete(function () {
-
-                });
-        }
-
-        //后退
-        $(document).on("click", ".top-nav-wrap .backBtn", function () {
-            window.history.back();
-        });
-    }
-
-    /**
-     * 侧边菜单
-     */
-    function sideNav($scope) {
-    }
-
-
-    /**
-     *
-     */
-    function pageCtrl($scope, modal) {
-        modal.closeAll();
-    }
-
-    angular.module('manageApp.main')
-        .controller('mainCtrl',  ["$scope",mainCtrl])
-        .controller('sideNav',  ["$scope",sideNav])
-        .controller('pageCtrl',  ["$scope","modal",pageCtrl])
-});
->>>>>>> 131c094fb42f0ae521b089da93162818f5b2c4ef
