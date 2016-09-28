38f024d8857fd5050078fc7327e5cbe1791d0fc8
kaindy7633 <kaindy7633@gmail.com>
2016-09-29 00:31:53 +0800

    conf.js问题

--- src/conf.js
+++ src/conf.js
@@ -1,13 +1,7 @@
 var conf={
   //serverPath:"http://120.76.232.188:8080/dt/",    //外部地址，系统api调用接口地址
-<<<<<<< HEAD
   //serverPath:"http://192.168.0.107:8080/dt/",    //系统api调用接口地址
   //serverPath:"http://localhost:8080/dt/",    //系统api调用接口地址
    serverPath:"",    //系统api调用接口地址
-=======
-  // serverPath:"http://192.168.0.107:8080/dt/",    //系统api调用接口地址
-  //serverPath:"http://localhost:8080/dt/",    //系统api调用接口地址
-  serverPath:"",    //系统api调用接口地址
->>>>>>> 48a1f0ed4864fd42c8096948644701847b5c7010
   ver:"1.0.0"
 };
