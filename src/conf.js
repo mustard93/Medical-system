48a1f0ed4864fd42c8096948644701847b5c7010
yjjewg11 <41049630@qq.com>
2016-09-28 23:57:17 +0800

    自动补齐调整

--- src/conf.js
+++ src/conf.js
@@ -1,7 +1,7 @@
 var conf={
   //serverPath:"http://120.76.232.188:8080/dt/",    //外部地址，系统api调用接口地址
-  serverPath:"http://192.168.0.107:8080/dt/",    //系统api调用接口地址
+  // serverPath:"http://192.168.0.107:8080/dt/",    //系统api调用接口地址
   //serverPath:"http://localhost:8080/dt/",    //系统api调用接口地址
-  // serverPath:"",    //系统api调用接口地址
+  serverPath:"",    //系统api调用接口地址
   ver:"1.0.0"
 };
