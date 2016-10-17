1a9e9bf8e88bd520125e7efc82e98f4a288cdcfd
yjjewg11 <41049630@qq.com>
2016-10-17 13:56:52 +0800

    0000795: 收货地址建议用京东、淘宝等输入地址的控件。增加省市区选择控件。

--- src/angular/modules/main/directives.js
+++ src/angular/modules/main/directives.js
@@ -2126,6 +2126,169 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
             }
         };
       }
+
+
+
+    //省市县的三级联动
+    function selectAddress ($http, $q, $compile) {
+        var cityURL, delay, templateURL;
+        delay = $q.defer();
+        templateURL = Config.tplPath+'tpl/cascading-select-address/cascading-select-address.html';
+        cityURL = Config.tplPath+'tpl/cascading-select-address/city.min.js';
+        $http.get(cityURL).success(function(data) {
+            return delay.resolve(data);
+        });
+        return {
+            restrict: 'A',
+            scope: {
+                p: '=',
+                a: '=',
+                c: '=',
+                d: '=',
+                ngModel: '='
+            },
+            link: function(scope, element, attrs) {
+                var popup;
+                popup = {
+                    element: null,
+                    backdrop: null,
+                    show: function() {
+                        return this.element.addClass('active');
+                    },
+                    hide: function() {
+                        this.element.removeClass('active');
+                        return false;
+                    },
+                    resize: function() {
+                        if (!this.element) {
+                            return;
+                        }
+                        this.element.css({
+                            top: -this.element.height() - 30,
+                            'margin-left': -this.element.width() / 2
+                        });
+                        return false;
+                    },
+                    focus: function() {
+                        $('[ng-model="d"]').focus();
+                        return false;
+                    },
+                    init: function() {
+                        element.on('click keydown', function() {
+                            popup.show();
+                            event.stopPropagation();
+                            return false;
+                        });
+//                        $(window).on('click', (function(_this) {
+//                            return function() {
+//                                return _this.hide();
+//                            };
+//                        })(this));
+                        this.element.on('click', function() {
+                            return event.stopPropagation();
+                        });
+                        return setTimeout((function(_this) {
+                            return function() {
+                                _this.element.show();
+                                return _this.resize();
+                            };
+                        })(this), 500);
+                    }
+                };
+                return delay.promise.then(function(data) {
+                    $http.get(templateURL).success(function(template) {
+                        var $template;
+                        $template = $compile(template)(scope);
+                        $('body').append($template);
+                        popup.element = $($template[2]);
+                        scope.provinces = data;
+                        return popup.init();
+                    });
+                    scope.aSet = {
+                        p: function(p) {
+                            scope.p = p;
+                            scope.c = null;
+                            scope.a = null;
+                            return scope.d = null;
+                        },
+                        c: function(c) {
+                            scope.c = c;
+                            scope.a = null;
+                            return scope.d = null;
+                        },
+                        a: function(a) {
+                            scope.a = a;
+                            scope.d = null;
+                            return popup.focus();
+                        }
+                    };
+                    scope.clear = function() {
+                        scope.p = null;
+                        scope.c = null;
+                        scope.a = null;
+                        return scope.d = null;
+                    };
+                    scope.submitAddress = function() {
+                        return popup.hide();
+                    };
+                    scope.$watch('p', function(newV) {
+                        var v, _i, _len, _results;
+                        if (newV) {
+                            _results = [];
+                            for (_i = 0, _len = data.length; _i < _len; _i++) {
+                                v = data[_i];
+                                if (v.p === newV) {
+                                    _results.push(scope.cities = v.c);
+                                }
+                            }
+
+
+                        }
+                        if(!scope.cities ){
+                            scope.cities=[];
+                            return scope.cities;
+                        }
+
+                        return _results;
+                    });
+                    scope.$watch('c', function(newV) {
+                        var v, _i, _len, _ref, _results;
+                        if (newV) {
+                            _ref = scope.cities;
+                            _results = [];
+
+                            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                                v = _ref[_i];
+                                if (v.n === newV) {
+                                    _results.push(scope.dists = v.a);
+                                }
+                            }
+                            return _results;
+                        } else {
+                            return scope.dists = [];
+                        }
+                    });
+                    return scope.$watch(function() {
+                        scope.ngModel = '';
+                        if (scope.p) {
+                            scope.ngModel += scope.p;
+                        }
+                        if (scope.c) {
+                            scope.ngModel += " " + scope.c;
+                        }
+                        if (scope.a) {
+                            scope.ngModel += " " + scope.a;
+                        }
+                        if (scope.d) {
+                            scope.ngModel += " " + scope.d;
+                        }
+                        return popup.resize();
+                    });
+                });
+            }
+        };
+    };
+    selectAddress.$inject = ["$http", "$q", "$compile"];
     /**
      * 加入项目
      */
@@ -2152,5 +2315,6 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
         .directive("chosen", chosen)
         .directive("formItem", formItem)
         .directive("autoComplete", autoComplete)
+        .directive("selectAddress", ["$http", "$q", "$compile",selectAddress])
         .directive("customConfig", customConfig)
 });
