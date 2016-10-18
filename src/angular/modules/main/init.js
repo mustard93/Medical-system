4b91c1c71cbaccb0b7e230198b1989fa98c805d2
yjjewg11 <41049630@qq.com>
2016-10-18 17:38:20 +0800

    easypiechart指令增加

--- src/angular/modules/main/init.js
+++ src/angular/modules/main/init.js
@@ -1,6 +1,6 @@
 /**
  * Created by hao on 15/11/11.
  */
-define('main/init', ['angular'], function () {
+define('main/init', ['angular','easypiechart'], function () {
     angular.module('manageApp.main', []);
 });
