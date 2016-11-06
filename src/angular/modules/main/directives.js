be6069cd5a8b175e97d7fa66a2fc07874168eb77
yjjewg11 <41049630@qq.com>
2016-11-06 21:19:32 +0800

    chosen参数中文兼容IE处理。 <select class="ipt pr-short-ipt" data-placeholder="选择规格" chosen default-empty="无" ng-model="formData.specification" select-source="rest/baseData/queryForSelectOptionByType"  params='{"type":"规格"}'> 0000776: [IE] 在IE浏览器下新建药械后不会在列表中显示

--- src/angular/modules/main/directives.js
+++ src/angular/modules/main/directives.js
@@ -1827,6 +1827,15 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
 
               require(['chosen'], function() {
                 if ($attrs.selectSource) {
+
+                  var _params={};
+                  if ($attrs.params) {
+                      if ($attrs.params.indexOf("{") === 0) {
+                            _params = $scope.$eval($attrs.params);
+                      }
+                  }
+
+
                   if (angular.isDefined($attrs.chosenAjax)) {
                     chosenObj = $element.chosen(chosenConfig);
                     var $chosenContainer = $element.next();
@@ -1871,15 +1880,15 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
                           if(maskObj)maskObj.hide();
                         }
                          maskObj=  proLoading($element, "chosen");
+
+                         _params.q=q;
+                         _params.id=ngModel.$viewValue;
                         requestQueue = $.ajax({
                             url: _url,
                             type: 'GET',
                             xhrFields:{withCredentials: true},
                             crossDomain:true,
-                            data: {
-                                q: q,
-                                id: ngModel.$viewValue
-                            },
+                            data: _params,
                             dataType: 'json',
                             success: function(_data) {
                                 if(maskObj)maskObj.hide();
@@ -2030,7 +2039,8 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
                             var tmp=$scope.$eval($attrs.ajaxIfEval);
                           if (!tmp) return;
                         }
-                        requestData($attrs.selectSource)
+
+                        requestData($attrs.selectSource,_params)
                           .then(function(results) {
                               var data = results[0];
                               var _options = '';
