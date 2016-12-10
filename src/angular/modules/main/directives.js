0405a833899b6b6a4e16b04f2693ce2928b83c64
yjjewg11 <41049630@qq.com>
2016-12-10 23:34:53 +0800

    增加指令 自动补全指令和药械查询自动补全。

--- src/angular/modules/main/directives.js
+++ src/angular/modules/main/directives.js
@@ -1587,199 +1587,53 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
                 $scope.minLength = 1;
                 $scope.searchStr = null;
 
-                var isNewSearchNeeded = function(newTerm, oldTerm) {
-                    return newTerm.length >= $scope.minLength && newTerm != oldTerm
-                };
+              require(['project/angucomplete'], function(angucomplete) {
+                    $scope.angucomplete1=new angucomplete($scope,elem,$parse, requestData, $sce, $timeout,ngModel);
 
-                $scope.processResults = function(responseData, str) {
-                    if (responseData && responseData.length > 0) {
-                        $scope.results = [];
+              });//angucomplete
 
-                        var titleFields = [];
-                        if ($scope.titleField && $scope.titleField != "") {
-                            titleFields = $scope.titleField.split(",");
-                        }
+            }//end link
+        };
+    };
 
-                        for (var i = 0; i < responseData.length; i++) {
-                            // Get title variables
-                            var titleCode = [];
+    /**
+     * 自动补全
+     */
+    function angucompleteMedical($parse, requestData, $sce, $timeout) {
+        return {
+            restrict: 'EA',
+            scope: {
+                "placeholder": "@",
+                "selectedItem": "=?",
+                "url": "@",
+                "titleField": "@",
+                "descriptionField": "@",
+                //"localData": "=?",
+                "searchFields": "@",
+                "matchClass": "@",
+                "ngDisabled": "=?"
+            },
+            require: "?^ngModel",
+            templateUrl: Config.tplPath + 'tpl/autocomplete-medicalStock.html',
+            link: function($scope, elem, attrs, ngModel) {
+                $scope.lastSearchTerm = null;
+                $scope.currentIndex = null;
+                $scope.justChanged = false;
+                $scope.searchTimer = null;
+                $scope.hideTimer = null;
+                $scope.searching = false;
+                $scope.pause = 300;
+                $scope.minLength = 1;
+                $scope.searchStr = null;
 
-                            for (var t = 0; t < titleFields.length; t++) {
-                                titleCode.push(responseData[i][titleFields[t]]);
-                            }
+                require(['project/angucomplete'], function(angucomplete) {
+                      $scope.angucomplete1=new angucomplete($scope,elem,$parse, requestData, $sce, $timeout,ngModel);
 
-                            var description = "";
-                            if ($scope.descriptionField) {
-                                description = responseData[i][$scope.descriptionField];
-                            }
-
-                            var text = titleCode.join(' ');
-                            if ($scope.matchClass) {
-                                var re = new RegExp(str, 'i');
-                                var strPart = text.match(re)[0];
-                                text = $sce.trustAsHtml(text.replace(re, '<span class="' + $scope.matchClass + '">' + strPart + '</span>'));
-                            }
-
-                            var resultRow = {
-                                id: responseData[i].id,
-                                title: text,
-                                description: description,
-                                //image: image,
-                                originalObject: responseData[i]
-                            };
-
-                            $scope.results[$scope.results.length] = resultRow;
-                        }
-
-
-                    } else {
-                        $scope.results = [];
-                    }
-                };
-
-                $scope.searchTimerComplete = function(str) {
-                    // Begin the search
-
-                    if (str.length >= $scope.minLength) {
-                        if ($scope.localData) {
-                            var searchFields = $scope.searchFields.split(",");
-
-                            var matches = [];
-
-                            for (var i = 0; i < $scope.localData.length; i++) {
-                                var match = false;
-
-                                for (var s = 0; s < searchFields.length; s++) {
-                                    match = match || (typeof $scope.localData[i][searchFields[s]] === 'string' && typeof str === 'string' && $scope.localData[i][searchFields[s]].toLowerCase().indexOf(str.toLowerCase()) >= 0);
-                                }
-
-                                if (match) {
-                                    matches[matches.length] = $scope.localData[i];
-                                }
-                            }
-
-                            $scope.searching = false;
-                            $scope.processResults(matches, str);
-
-                        } else {
-                            requestData($scope.url, {
-                                    q: str
-                                })
-                                .then(function(results) {
-                                    var data = results[0];
-                                    $scope.searching = false;
-                                    $scope.processResults(data, str);
-                                })
-                                .catch(function(error) {
-                                    $scope.searching = false;
-                                    console.error(error);
-                                });
-                        }
-                    }
-                };
-
-                $scope.hideResults = function() {
-                    $scope.hideTimer = $timeout(function() {
-                        $scope.showDropdown = false;
-                    }, $scope.pause);
-                };
-
-                $scope.resetHideResults = function() {
-                    if ($scope.hideTimer) {
-                        $timeout.cancel($scope.hideTimer);
-                    }
-                };
-
-                $scope.hoverRow = function(index) {
-                    $scope.currentIndex = index;
-                };
-
-                $scope.keyPressed = function(event) {
-                    if (!(event.which == 38 || event.which == 40 || event.which == 13)) {
-                        if (!$scope.searchStr || $scope.searchStr == "") {
-                            $scope.showDropdown = false;
-                            $scope.lastSearchTerm = null
-                        } else if (isNewSearchNeeded($scope.searchStr, $scope.lastSearchTerm)) {
-                            $scope.lastSearchTerm = $scope.searchStr;
-                            $scope.showDropdown = true;
-                            $scope.currentIndex = -1;
-                            $scope.results = [];
-
-                            if ($scope.searchTimer) {
-                                $timeout.cancel($scope.searchTimer);
-                            }
-
-                            $scope.searching = true;
-
-                            $scope.searchTimer = $timeout(function() {
-                                $scope.searchTimerComplete($scope.searchStr);
-                            }, $scope.pause);
-                        }
-                    } else {
-                        event.preventDefault();
-                    }
-                };
-
-                $scope.selectResult = function(result) {
-                    if ($scope.matchClass) {
-                        result.title = result.title.toString().replace(/(<([^>]+)>)/ig, '');
-                    }
-                    $scope.searchStr = $scope.lastSearchTerm = result.title;
-                    $scope.selectedItem = result;
-                    $scope.showDropdown = false;
-                    $scope.results = [];
-                    ngModel && ngModel.$setViewValue(result.id);
-                };
-
-                var inputField = elem.find('input');
-
-                inputField.on('keyup', $scope.keyPressed);
-
-                elem.on("keyup", function(event) {
-                    if (event.which === 40) {
-                        if ($scope.results && ($scope.currentIndex + 1) < $scope.results.length) {
-                            $scope.currentIndex++;
-                            $scope.$apply();
-                            event.preventDefault;
-                            event.stopPropagation();
-                        }
-
-                        $scope.$apply();
-                    } else if (event.which == 38) {
-                        if ($scope.currentIndex >= 1) {
-                            $scope.currentIndex--;
-                            $scope.$apply();
-                            event.preventDefault;
-                            event.stopPropagation();
-                        }
-
-                    } else if (event.which == 13) {
-                        if ($scope.results && $scope.currentIndex >= 0 && $scope.currentIndex < $scope.results.length) {
-                            $scope.selectResult($scope.results[$scope.currentIndex]);
-                            $scope.$apply();
-                            event.preventDefault;
-                            event.stopPropagation();
-                        } else {
-                            $scope.results = [];
-                            $scope.$apply();
-                            event.preventDefault;
-                            event.stopPropagation();
-                        }
-
-                    } else if (event.which == 27) {
-                        $scope.results = [];
-                        $scope.showDropdown = false;
-                        $scope.$apply();
-                    } else if (event.which == 8) {
-                        $scope.selectedItem = null;
-                        $scope.$apply();
-                    }
-                });
+                });//angucomplete
 
             }
         };
     };
-    angucomplete.$inject = ["$parse", "requestData", "$sce", "$timeout"];
 
     /**
      * checkbox
@@ -2813,7 +2667,9 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
         .directive("selectAsync", selectAsync)
         .directive("relativeSelect", relativeSelect)
         .directive("chart", eChart)
-        .directive("angucomplete", angucomplete)
+        .directive("angucomplete", ["$parse", "requestData", "$sce", "$timeout",angucomplete])
+        .directive("angucompleteMedical", ["$parse", "requestData", "$sce", "$timeout",angucompleteMedical])
+
         .directive("checkboxGroup", checkboxGroup)
         .directive("chosen", chosen)
         .directive("formItem", formItem)
