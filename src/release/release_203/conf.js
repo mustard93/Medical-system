08801d5c98ad89e4570ca5f9a25c329df21a1f32
yjjewg11 <41049630@qq.com>
2016-12-15 16:15:56 +0800

    已修复支持配置http和https

--- src/release/release_203/conf.js
+++ src/release/release_203/conf.js
@@ -1,9 +1,9 @@
 var conf={
   //stopIntervalNotice:false,//false|true.是否停止监听消息
   //serverPath:"http://120.76.232.188:8080/dt/",    //外网地址。
-  serverPath:"http://192.168.0.203:8080/dt/",    //203 测试环境
+  // serverPath:"http://192.168.0.203:8080/dt/",    //203 测试环境
   //serverPath:"http://localhost:8080/dt/",    //本地开发环境
-  // serverPath:"/dt/",    //接口和web同一个服务器
+  serverPath:"/dt/",    //接口和web同一个服务器
   //serverPath:"",    //同一个服务器和应用上下文
   ver:"1.0.0"
 };
