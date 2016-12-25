5b144faf0e9eeb2fe9f88e69ad9ecb9ece0d398d
yjjewg11 <41049630@qq.com>
2016-12-25 17:59:57 +0800

    修复库存明细，库存成本金额显示不正确bug。

--- src/angular/modules/main/services.js
+++ src/angular/modules/main/services.js
@@ -540,6 +540,8 @@ function alertOk($rootScope, modal) {
            //乘法
            numberMul:function (arg1,arg2)
            {
+              if(!arg1)arg1=0;
+                if(!arg2)arg2=0;
                var m=0,s1=arg1.toString(),s2=arg2.toString();
                try{m+=s1.split(".")[1].length}catch(e){};
                try{m+=s2.split(".")[1].length}catch(e){};
@@ -547,6 +549,8 @@ function alertOk($rootScope, modal) {
            },
           //加法
           numberAdd:function(arg1,arg2){
+            if(!arg1)arg1=0;
+              if(!arg2)arg2=0;
               var r1,r2,m;
               try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0};
               try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0};
@@ -555,7 +559,8 @@ function alertOk($rootScope, modal) {
           },
           //减法
           numberSub:function(arg1,arg2){
-               var r1,r2,m,n;
+            if(!arg1)arg1=0;
+              if(!arg2)arg2=0;
                try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0};
                try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0};
                m=Math.pow(10,Math.max(r1,r2));
