6417eaf3c52b1d183da0b21f2637fe4af216913a
yjjewg11 <41049630@qq.com>
2017-01-07 22:35:48 +0800

    0000879: 输入客户名后删除以正常速度（1S删除一个字）删除完所有字后会自动再带出第一个字

--- src/angular/modules/main/directives.js
+++ src/angular/modules/main/directives.js
@@ -1964,6 +1964,9 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
                                 $element.trigger("chosen:updated");
                                 var keyRight = $.Event('keydown');
                                 keyRight.which = 39;
+                                //0000879: 输入客户名后删除以正常速度（1S删除一个字）删除完所有字后会自动再带出第一个字
+                                // console.log(q);
+                                  searchStr=q;
                                 $input.val(q).trigger(keyRight);
 
                                 if (_data.data.length > 0) {
@@ -2035,11 +2038,13 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
                       $chosenContainer.find('.no-results').hide();
 
                       var q = $.trim(field.val());
-
-                      if (!q && searchStr == q) {
+                      //0000879: 输入客户名后删除以正常速度（1S删除一个字）删除完所有字后会自动再带出第一个字
+                      // if (!q && searchStr == q) {
+                      //   return false;
+                      // }
+                      if (searchStr == q) {
                         return false;
                       }
-
                       typing = true;
 
                       if ($scope.searchTimer) {
