5fce9c5c2486dcbc61dbf75218860760ca560a17
阿振sc <3494688@qq.com>
2017-01-09 16:07:47 +0800

    修复bug：0001350: 首营品种：药械名太长时应该只显示一排， 用省略号的形式

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -915,26 +915,25 @@ define('project/controllers', ['project/init'], function() {
       // 当用户选择某条目的生产批号后，将该条目设置为已选择状态
       $scope.choiseProductionBatch = function (item,stockBatchsItem,selectData) {
         if(stockBatchsItem&&!stockBatchsItem.quantity){
-                //库存批次数量，满足则数量设置为计划数量。
-                if(!item.quantity)item.quantity=0;
+          //库存批次数量，满足则数量设置为计划数量。
+          if(!item.quantity)item.quantity=0;
 
-                stockBatchsItem.quantity=item.planQuantity-item.quantity;
+          stockBatchsItem.quantity=item.planQuantity-item.quantity;
 
-                if(selectData){
-                    if(!selectData.note)selectData.note={};
-                    if(!selectData.note.salesQuantity)selectData.note.salesQuantity=0;
-                }
+          if(selectData){
+              if(!selectData.note)selectData.note={};
+              if(!selectData.note.salesQuantity)selectData.note.salesQuantity=0;
+          }
 
-                if(selectData&&selectData.note&&selectData.note.salesQuantity){
-                  //批次库存不满足计划销售数量
-                  if(stockBatchsItem.quantity>selectData.note.salesQuantity){
-                    stockBatchsItem.quantity= selectData.note.salesQuantity;
-
-                  }
-                }else{//未获取到批次数量
-                  stockBatchsItem.quantity=null;
-                }
-
+          if(selectData&&selectData.note&&selectData.note.salesQuantity){
+            //批次库存不满足计划销售数量
+            if(stockBatchsItem.quantity>selectData.note.salesQuantity){
+              stockBatchsItem.quantity= selectData.note.salesQuantity;
+              $scope.item.handleFlag=true;
+            }
+          }else{//未获取到批次数量
+            stockBatchsItem.quantity=null;
+          }
         }
 
       };
