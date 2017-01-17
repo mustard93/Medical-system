b4c687a9edbbb8b7ed143bcbd70ca62f30324d30
yjjewg11 <41049630@qq.com>
2017-01-17 11:13:13 +0800

    web增加客服咨询功能。

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -4,6 +4,45 @@
 define('project/controllers', ['project/init'], function() {
 
 
+  /**
+   * [saleReturnMedicalItemController 新建销退单药品列表tr控制器]
+   * @param  {[type]} $scope [description]
+   * @return {[type]}        [description]
+   */
+  function imTaobaoCtr ($scope,requestData,alertError,$rootScope) {
+    //弹出与某人聊天.toUserId 聊天 人的userid
+    $scope.openIm=function(toUserId,toUserName){
+      var imTaobaoUserInfo=  $rootScope.MyImTaobaoUserInfo;
+      if(!imTaobaoUserInfo){//没有帐号就获取帐号
+        imTaobaoUserInfo_getMy(function(){$scope.openIm(toUserId)});
+        return ;
+      }
+      if(!toUserId)toUserId="";
+      if(!toUserName)toUserName="";
+      var param="?uid="+imTaobaoUserInfo.userid+"&to="+toUserId+"&toUserName="+toUserName+"&appkey=23588140&pwd="+imTaobaoUserInfo.password+"&fullscreen";
+      window.open('imtaobao/kit.html'+param, 'webcall', 'toolbar=no, status=no,scrollbars=0,resizable=0,menubar＝0,location=0,width=700,height=530');
+
+    }
+
+    //获取我的聊天帐号信息
+    function imTaobaoUserInfo_getMy(callback) {
+      var url='rest/authen/imTaobaoUserInfo/getMy.json';
+      requestData(url)
+        .then(function (results) {
+          var _data = results[0];
+          $rootScope.MyImTaobaoUserInfo= results[0];
+          if(callback){
+            callback();
+          }
+        })
+        .catch(function (error) {
+          alertError(error || '出错');
+        });
+    }//imTaobaoUserInfo_getMy
+
+
+
+  }//imTaobaoCtr
 
 
       /**
@@ -2064,6 +2103,9 @@ function SalesOrderDetailsController ($scope, $timeout, alertOk, alertError, req
 
 
     angular.module('manageApp.project')
+
+
+    .controller('imTaobaoCtr', ['$scope',"requestData",'alertError',"$rootScope", imTaobaoCtr])
     .controller('saleReturnMedicalItemController', ['$scope', saleReturnMedicalItemController])
     .controller('saleReturnOrderAddController', ["$scope", "$rootScope", "modal", saleReturnOrderAddController])
     .controller('mainCtrlProject',  ["$scope","$rootScope","$http", "$location", "store","utils","modal","OPrinter","UICustomTable","bottomButtonList","saleOrderUtils", mainCtrlProject])
