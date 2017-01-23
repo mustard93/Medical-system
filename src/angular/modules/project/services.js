6387b1948826addbb26c111a1437909be179d054
宋娟songjuan <1490590465@qq.com>
2017-01-23 09:45:30 +0800

    地址信息修改

--- src/angular/modules/project/services.js
+++ src/angular/modules/project/services.js
@@ -302,7 +302,7 @@ define('project/services', ['project/init'], function () {
               var tmp;
               tmp = utils.numberDiv(item.taxRate,100);
               tmp = 1 + tmp;
-              tmp = utils.numberDiv(item.purchasePrice,tmp);
+              tmp = utils.numberDiv(item.price,tmp);
               return tmp;
             },
             //原币金额（无税金额） item.price*(1-item.taxRate)*item.quantity
@@ -319,7 +319,7 @@ define('project/services', ['project/init'], function () {
               //item.purchasePrice*item.quantity
               // var tmp=utils.numberMul(item.taxPrice,item.quantity);
               var tmp;
-              tmp = utils.numberMul(item.purchasePrice, item.quantity);
+              tmp = utils.numberMul(item.price, item.quantity);
               return tmp;
             }
           };//tmpObj
