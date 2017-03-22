f13e3cddf2fe641af747d75df63b0606edaf6303
阿振sc <3494688@qq.com>
2017-03-22 15:04:26 +0800

    完成manage管理模块中WMS实例管理前端应用

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -4919,27 +4919,65 @@ define('project/controllers', ['project/init'], function() {
    * @return {[type]} [description]
    */
   function infrastructureController ($scope) {
-    $scope.buildMapping = function (obj, key, val) {
 
-      console.log(obj);
-      console.log(key);
-      console.log(val);
+    $scope.buildMapping = function (objName, keyName, valName) {
 
-      if (angular.isObject(obj)) {
-        var _keys = Object.keys(obj);
-        // console.log(_keys);
-        angular.forEach(_keys, function (item, index) {
-          if (item === key) {
-            return;
-          } else {
-            // $scope.formData.warehouseTypeMapping = {
-            //   key:val
-            // };
-            $scope.formData.warehouseTypeMapping[key] = val;
+      if ($scope.formData[keyName] && $scope.formData[valName]) {
+        if ($scope.formData[objName]) {
+          // 检查key
+          var _keys = Object.keys($scope.formData[objName]);
+          if (_keys.indexOf($scope.formData[keyName]) != -1) {
+            alert('选择的属性已存在'); return;
           }
-        });
-        console.log($scope.formData.warehouseTypeMapping);
+
+          // 检查value
+          for (var i in $scope.formData[objName]) {
+            if ($scope.formData[objName][i] == $scope.formData[valName]) {
+              alert('选择的值已存在'); return;
+            }
+          }
+        }
+
+        $scope.formData[objName][$scope.formData[keyName]] = $scope.formData[valName];
+
+        if ($scope.formData[objName]) {
+          $scope.formData[keyName] = '';
+          $scope.formData[valName] = '';
+        }
       }
+    };
+
+    // 初始化扩展属性
+    $scope.initExtendedAttribute = function (extendedAttrObj) {
+
+      if (angular.isObject(extendedAttrObj)) {
+        $scope.extendedAttrList = [];
+        for (var i in extendedAttrObj) {
+          var _tmp = {name:i, val:extendedAttrObj[i]};
+          $scope.extendedAttrList.push(_tmp);
+        }
+
+        // console.log($scope.extendedAttrList);
+      }
+    };
+
+    // 扩展属性选择后操作
+    $scope.handleChoiseEvent = function (extendedAttrList, key) {
+      angular.forEach(extendedAttrList, function (item, index) {
+        if (key == item.name) { $scope.formData.extendedAttributeValue = item.val; }
+      });
+    };
+
+    // 保存扩展属性更改
+    $scope.saveExtendedAttribute = function () {
+      // console.log($scope.formData.extendedAttributeValue);
+      for (var i in $scope.formData.extendedAttribute) {
+        // console.log(i+' => '+$scope.formData.extendedAttributeKey.name);
+        if (i == $scope.formData.extendedAttributeKey.name) {
+          $scope.formData.extendedAttribute[i] = $scope.formData.extendedAttributeValue;
+        }
+      }
+      // console.log($scope.formData);
     };
   }
 
