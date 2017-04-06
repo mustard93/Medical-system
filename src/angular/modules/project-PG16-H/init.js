48497bc26726468642dbeffb743dc5075c8427ab
yjjewg11 <41049630@qq.com>
2017-04-06 15:56:18 +0800

    配置变更

--- src/angular/modules/project-PG16-H/init.js
+++ src/angular/modules/project-PG16-H/init.js
@@ -1,6 +1,6 @@
 /**
  * Created by hao on 15/11/11.
  */
-define('project/init', ['angular'], function () {
-    angular.module('manageApp.project', []);
+define('project-PG16-H/init', ['angular'], function () {
+    angular.module('manageApp.project-PG16-H', []);
 });
