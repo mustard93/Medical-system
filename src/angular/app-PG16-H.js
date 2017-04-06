70912a72d986fbacf0cc15d5023f0a7dc0851076
阿振sc <3494688@qq.com>
2017-04-06 14:05:49 +0800

    PSD环境配置

--- src/angular/app-PG16-H.js
+++ src/angular/app-PG16-H.js
@@ -11,153 +11,111 @@
     }
     require.dir = loaderScript.src.match(/[^?#]*\//)[0];
 
-
-
     //ueditor编辑器路径配置
     window.UEDITOR_HOME_URL=require.dir+"libs/ueditor1_4_3_3-utf8-jsp/";
-
 
     if (!Config.tplPath) {
       Config.tplPath = "";
     }
 
-
 }());
     // window.UMEDITOR_HOME_URL=Config.serverPath;//编辑器需要 配置服务器地址
 require.config({
 
-  map: {
-        "*": {
-            "css": "../css.min"
-        }
+    map: {
+      "*": {
+        "css": "../css.min"
+      }
     },
     waitSeconds: 0,
     baseUrl: require.dir + 'modules',
     paths: {
-
-          'resizableColumns': '../libs/resizableColumns/jquery.resizableColumns.min',     //jQuery拖动调整表格列宽度
-          'store': '../libs/store.min',     //本地存储
-          'ZeroClipboard': '../libs/ueditor1_4_3_3-utf8-jsp/third-party/zeroclipboard/ZeroClipboard',//html编辑器
-
-        'ueditor': '../libs/ueditor1_4_3_3-utf8-jsp/ueditor.all.min',//html编辑器
-        'ueditor_config': '../libs/ueditor1_4_3_3-utf8-jsp/ueditor.config',//编辑器。require 不支持。
-        'ueditor_lang': '../libs/ueditor1_4_3_3-utf8-jsp/lang/zh-cn/zh-cn',//编辑器
-        'CanvasBusinessFlow': '../libs/project/CanvasBusinessFlow.min',//业务单图展示
-        'WorkflowProcess': '../libs/project/WorkflowProcess.min',//工作流
-          'CanvasTreeLayout':"../libs/project/CanvasTreeLayout.min",//流程图布局定义
-        'LodopFuncs': '../libs/LodopFuncs_amd',     //打印功能
-        'JTopo': '../libs/jtopo-0.4.8.min',
-        'jQuery': '../libs/jquery.min',                         //jQuery
-        'underscore': '../libs/underscore/underscore-1.5.2.min',
-        'moment': '../libs/moment.min',                         //日历插件
-        'echarts': '../libs/echarts/echarts-3.2.3.min',         // echars v3.2.3
-        'chosen': '../libs/chosen.jquery.pg.min',                  //下拉筛选插件.已修改源码
-        'angular': '../libs/angular.min',                           //Angular
-        'ngRoute': '../libs/angular-route.min',                     //路由
-        'autocomplete': '../libs/jquery.autocomplete.min',      //自动补齐
-        'bootstrap': '../libs/bootstrap.min',                   //bootstrap
-        'jquery-ui': '../libs/jquery-ui.min',
-        'nicescroll': '../libs/jquery-nicescroll.min',          //滚动条美化
-        'easypiechart': '../libs/easypiechart/angular.easypiechart',    //jQuery饼图
-        'icheck': '../libs/jquery.icheck.min',                  //checkbox美化
-        'morris': '../libs/morris-chart/morris.min',                         //日历插件
-        'toastr': '../libs/toastr/toastr.min',   //提示窗口
-        'raphael': '../libs/morris-chart/raphael.min',                         //日历插件
-        'clndr': '../libs/calendar/clndr.min'                             //日历插件
-    },
+      'resizableColumns': '../libs/resizableColumns/jquery.resizableColumns.min',     //jQuery拖动调整表格列宽度
+      'store': '../libs/store.min',     //本地存储
+      'ZeroClipboard': '../libs/ueditor1_4_3_3-utf8-jsp/third-party/zeroclipboard/ZeroClipboard',//html编辑器
+      'ueditor': '../libs/ueditor1_4_3_3-utf8-jsp/ueditor.all.min',//html编辑器
+      'ueditor_config': '../libs/ueditor1_4_3_3-utf8-jsp/ueditor.config',//编辑器。require 不支持。
+      'ueditor_lang': '../libs/ueditor1_4_3_3-utf8-jsp/lang/zh-cn/zh-cn',//编辑器
+      'CanvasBusinessFlow': '../libs/project/CanvasBusinessFlow.min',//业务单图展示
+      'WorkflowProcess': '../libs/project/WorkflowProcess.min',//工作流
+      'CanvasTreeLayout':"../libs/project/CanvasTreeLayout.min",//流程图布局定义
+      'LodopFuncs': '../libs/LodopFuncs_amd',     //打印功能
+      'JTopo': '../libs/jtopo-0.4.8.min',
+      'jQuery': '../libs/jquery.min',                         //jQuery
+      'underscore': '../libs/underscore/underscore-1.5.2.min',
+      'moment': '../libs/moment.min',                         //日历插件
+      'echarts': '../libs/echarts/echarts-3.2.3.min',         // echars v3.2.3
+      'chosen': '../libs/chosen.jquery.pg.min',                  //下拉筛选插件.已修改源码
+      'angular': '../libs/angular.min',                           //Angular
+      'ngRoute': '../libs/angular-route.min',                     //路由
+      'autocomplete': '../libs/jquery.autocomplete.min',      //自动补齐
+      'bootstrap': '../libs/bootstrap.min',                   //bootstrap
+      'nicescroll': '../libs/jquery-nicescroll.min',          //滚动条美化
+      'toastr': '../libs/toastr/toastr.min',   //提示窗口
+  },
     shim: {
       'ueditor_config': {
-          deps: ['jQuery']
+        deps: ['jQuery']
       },
-      // 'ZeroClipboard': {
-      //       deps: ['jQuery'],
-      //     exports: 'ZeroClipboard'
-      // },
       'ueditor': {
-          deps: ['jQuery','ZeroClipboard'],
-          init:function(ZeroClipboard){
-               //导出到全局变量，供ueditor使用
-               window.ZeroClipboard = ZeroClipboard;
-           }
+        deps: ['jQuery','ZeroClipboard'],
+        init:function(ZeroClipboard){
+             //导出到全局变量，供ueditor使用
+             window.ZeroClipboard = ZeroClipboard;
+         }
       },
 //,'css!../libs/umeditor1_2_2/themes/default/css/umeditor.min.css'
       'ueditor_lang': {
-          deps: ['ueditor','ueditor_config'],
-            exports: 'UM'
+        deps: ['ueditor','ueditor_config'],
+        exports: 'UM'
       },
-        'JTopo': {
-            exports: 'JTopo'
-        },
-        'jQuery': {
-            exports: 'jQuery'
-        },
-        'resizableColumns': {
-            deps: ['jQuery','css!../libs/resizableColumns/jquery.resizableColumns.css']
-        },
-
-
-        'underscore': {
-            exports: '_'
-        },
-        'toastr': {
-            deps: ['jQuery']
-        },
-        'chosen': {
-            deps: ['jQuery']
-        },
-        'echarts': {
-            exports: 'echarts'
-        },
-
-        'angular': {
-            deps: ['jQuery'],
-            exports: 'angular'
-        },
-        'ngRoute': {
-          deps: ['angular'],
-          exports: 'ngRoute'
-        },
-        'autocomplete': {
-          deps: ['jQuery'],
-          exports: 'autocomplete'
-        },
-        'bootstrap': {
-            deps: ['jQuery'],
-            exports: 'bootstrap'
-        },
-        'jquery-ui': {
-            deps: ['jQuery'],
-            exports: 'jquery-ui'
-        },
-        'nicescroll': {
-            deps: ['jQuery'],
-            exports: 'nicescroll'
-        },
-        'easypiechart': {
-            deps: ['jQuery'],
-            exports: 'easypiechart'
-        },
-        'icheck': {
-            deps: ['jQuery'],
-            exports: 'icheck'
-        },
-        'morris': {
-            deps: ['raphael'],
-            exports: 'morris'
-        },
-        'clndr': {
-            deps: ['jQuery'],
-            exports: 'clndr'
-        }
+      'JTopo': {
+        exports: 'JTopo'
+      },
+      'jQuery': {
+          exports: 'jQuery'
+      },
+      'resizableColumns': {
+          deps: ['jQuery','css!../libs/resizableColumns/jquery.resizableColumns.css']
+      },
+      'underscore': {
+          exports: '_'
+      },
+      'toastr': {
+          deps: ['jQuery']
+      },
+      'chosen': {
+          deps: ['jQuery']
+      },
+      'echarts': {
+          exports: 'echarts'
+      },
+      'angular': {
+        deps: ['jQuery'],
+        exports: 'angular'
+      },
+      'ngRoute': {
+        deps: ['angular'],
+        exports: 'ngRoute'
+      },
+      'autocomplete': {
+        deps: ['jQuery'],
+        exports: 'autocomplete'
+      },
+      'bootstrap': {
+        deps: ['jQuery'],
+        exports: 'bootstrap'
+      },
+      'nicescroll': {
+        deps: ['jQuery'],
+        exports: 'nicescroll'
+      }
     },
     urlArgs: ''
 });
 
 require(['bootstrap',
-         'jquery-ui',
-         'nicescroll',
-         'easypiechart',
-         'icheck'], function() {});
+         'nicescroll',], function() {});
 
 //@ifdef !production
 define('manageApp.template', ['angular'], function () {
@@ -178,7 +136,7 @@ define('manageApp', [
        'manageApp.modal',
        'manageApp.main',
        'manageApp.project',
-	   'manageApp.project-PG16-H',
+	     'manageApp.project-PG16-H',
        'manageApp.upload',
        'datePicker'
 ], function () {
@@ -188,7 +146,7 @@ define('manageApp', [
         'manageApp.modal',
         'manageApp.main',
         'manageApp.project',
-		'manageApp.project-PG16-H',
+		    'manageApp.project-PG16-H',
         'manageApp.upload',
         'datePicker',
         'easypiechart'
@@ -196,9 +154,6 @@ define('manageApp', [
 });
 
 require(['manageApp','ngRoute'], function (app) {
-
-
-
     app.config(['$routeProvider', '$templateRequestProvider','$sceDelegateProvider',
        function ($routeProvider, $templateRequestProvider,$sceDelegateProvider) {
 
