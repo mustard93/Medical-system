e113adbac5b61113539fd20777c23e6460a6bbb3
宋娟songjuan <1490590465@qq.com>
2017-05-11 15:04:03 +0800

    解决调拨单数量限制输入问题

--- src/angular/modules/project-PG16-H/directives.js
+++ src/angular/modules/project-PG16-H/directives.js
@@ -46,13 +46,26 @@ define('project-PG16-H/directives', ['project-PG16-H/init'], function () {
     };
   }
 
+
+  // 根据返回状态显示小圆点样式，鼠标移入显示状态提示。
+  /**
+     *
+    	* @Description: 鼠标移入显示状态提示，
+    	* @author 宋娟
+    	* @date 2017年5月10日 下午4:32:59
+     */
+
+     	   //  关键步骤：
+     	    //1.传入参数:showOrderStatus(后台返回的状态值)，style(控制样式)。
+
   function showStatus(utils){
     return{
       restrict: 'A',
         link: function ($scope, $element, $attrs) {
-          // $element.css({'position':'relative'});
+          $element.css({'position':'relative'});
+          $element.addClass('showStatus');
           var orderStatus=$attrs.showOrderStatus;
-          var style=$attrs.style;
+          var style=$attrs.showOrderStyle;
           // 截取返回状态值得首尾字符，用于后续判断显示颜色
           var orderStartColor=orderStatus.substring(1,0);
           var orderEndColor=orderStatus.substr(orderStatus.length-1,1);
@@ -64,7 +77,7 @@ define('project-PG16-H/directives', ['project-PG16-H/init'], function () {
           }else if(orderEndColor=='中' || orderStartColor=='部'){
             bgColor=' bg-freeze-orange';
           }
-          var circle="<span class='circle-status"+bgColor+"'><span class='purchaseorder-buyer-info' style='"+style+"'><i class='block pd-c-s text-normal'><em class='inline-block'>"+orderStatus
+          var circle="<span class='circle-status"+bgColor+"'><span class='purchaseorder-buyer-info' style='"+style+"'><i class='text-normal pd-c-s' style='height:20px;'><em class='inline-block'>"+orderStatus
           +"</em></i></sapn></span>"
           $element.append(circle);
         }
