3cca9736e8f725be5dc524fb8ee73eb095da8fd0
venray <2469415004@qq.com>
2017-05-12 10:21:18 +0800

    0002341: 经销商平台， 登录未绑定的用户跳转页面不正确

--- src/dt/release/release_203/conf.js
+++ src/dt/release/release_203/conf.js
@@ -1,5 +1,6 @@
 var conf={
-  applyBindUrl:"user.html#/reg/bind_done.html",
+  // applyBindUrl:"user.html#/reg/bind_done.html",
+  applyBindUrl:"user1.html#/reg/bind_done.html",//
   serverPath:"/dt/", 
   ver:"1.0.0"
 };
