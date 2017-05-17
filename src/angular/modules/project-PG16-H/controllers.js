f905ad93b6b3952b461fa193ef73680c79f4caa6
阿振sc <3494688@qq.com>
2017-05-17 09:11:41 +0800

    GS1条码打印完成100%

--- src/angular/modules/project-PG16-H/controllers.js
+++ src/angular/modules/project-PG16-H/controllers.js
@@ -215,6 +215,7 @@ define('project-PG16-H/controllers', ['project-PG16-H/init'], function() {
       addDataItem.planQuantity;
       return true;
     };
+
   }
 
   function medicalStockStrategyCtrl ($scope, watchFormChange, requestData, utils, alertError, alertWarn) {
@@ -2410,6 +2411,57 @@ define('project-PG16-H/controllers', ['project-PG16-H/init'], function() {
     }
   }
 
+  // SPD商品管理中GS1条码打印控制器
+  function cfgGoodsBarcodeCtroller ($scope, requestData, utils) {
+
+    var _url = 'rest/authen/gs1Barcode/get';
+
+    // 获取商品条码
+    $scope.getGoodsBarcode = function (barcode) {
+
+      if (barcode) {
+        var _data = {
+              "barcode": barcode,
+              "barcodeType": "一段式"
+            };
+
+        requestData(_url, _data, 'POST', 'parameter-body')
+        .then(function (results) {
+          if (results[1].code === 200) {
+            $scope.goodsBarcode = results[1].data;   // 商品条码
+            $scope.goodsFullBarcode = results[1].data;   // 完整的商品条码，包含批号、数量
+            $scope.scopeData.medicalType = '一段式';   // 设置默认的条码选择样式
+          }
+        })
+        .catch(function (error) {
+          if (error) { throw new Error(error || '出错'); }
+        });
+      }
+    };
+
+    // 请求包含批号和数量的完整的条码
+    $scope.getFullGoodsBarcode = function (scopeData) {
+      if (scopeData) {
+        var _data = {
+          "barcode": scopeData.barcode,
+          "quantity": scopeData.quantity,
+          "productionBatch": scopeData.productionBatch,
+          "validTill": scopeData.validTill,
+          "barcodeType": scopeData.medicalType
+        };
+        requestData(_url, _data, 'POST', 'parameter-body')
+        .then(function (results) {
+          if (results[1].code === 200) {
+            $scope.goodsFullBarcode = results[1].data;   // 完整的商品条码，包含批号、数量
+          }
+        })
+        .catch(function (error) {
+          if (error) { throw new Error(error || '出错'); }
+        });
+      }
+    };
+  }
+
   angular.module('manageApp.project-PG16-H')
   .controller('mainCtrlProjectPG16H',  ["$scope","$rootScope","$http", "$location", "store","utils","modal","OPrinter","UICustomTable","bottomButtonList","saleOrderUtils","purchaseOrderUtils","requestPurchaseOrderUtils","queryItemCardButtonList","customMenuUtils", mainCtrlProjectPG16H])
   .controller('medicalStockCtrl', ['$scope', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', medicalStockCtrl])
@@ -2425,5 +2477,6 @@ define('project-PG16-H/controllers', ['project-PG16-H/init'], function() {
   .controller('storeRoomController', ['$scope', 'requestData', 'alertError', 'alertOk', storeRoomController])
   .controller('purchaseReturnController', ['$scope', 'modal', 'alertWarn', 'watchFormChange', 'requestData', '$rootScope', 'alertOk', 'utils', purchaseReturnController])
   .controller('checkUpController', ['$scope', 'requestData', 'utils', 'modal', checkUpController])
-  .controller('pickBillOrderController', ['$scope', 'requestData', 'utils', 'modal', pickBillOrderController]);
+  .controller('pickBillOrderController', ['$scope', 'requestData', 'utils', 'modal', pickBillOrderController])
+  .controller('cfgGoodsBarcodeCtroller', ['$scope', 'requestData', 'utils', cfgGoodsBarcodeCtroller]);
 });
