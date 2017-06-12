2efd97c09e9ae18393923f6be553a40fa4df5760
阿振sc <3494688@qq.com>
2017-06-12 19:57:16 +0800

    提交代码

--- src/angular/modules/main/components.js
+++ src/angular/modules/main/components.js
@@ -3,14 +3,19 @@
  *  create by liuzhen
  */
 define('main/components', ['main/init'], function () {
+  function testComponent () {
+    return {
+      template: '<div>Test - Components</div>',
+      bindings: {
+        test: '='
+      },
+      controller: ['$scope', function ($scope) {
+        console.log($scope);
+      }]
+    }
+  }
+
+
   angular.module('manageApp.main')
-  .component('testComponent', {
-    template: '<div>Test - Components</div>',
-    bindings: {
-      test: '='
-    },
-    controller: ['$scope', function ($scope) {
-      console.log($scope);
-    }]
-  });
+  .component('testComponent', [testComponent]);
 });
