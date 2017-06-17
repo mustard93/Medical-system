e76f1ec0df0265f7eabfdf83a866eea96c117f54
yjjewg11 <41049630@qq.com>
2017-06-17 14:59:17 +0800

    46 经销商平台，销售出库单，只有第三个快递有数据，点击查看物流信息时，切换快递，变成第一个快递有物流信息

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -5234,6 +5234,30 @@ define('project/controllers', ['project/init'], function() {
    * @param  {[type]} $scope [description]
    * @return {[type]}        [description]
    */
+  // function getAllExpressController ($scope, requestData) {
+  //
+  //   var _kuaidiSet = $scope.dialogData.kuaidiSet;
+  //   var _url = 'rest/index/kuaidi/query2.json';
+  //   $scope.expressInfoArray = [];
+  //
+  //   angular.forEach(_kuaidiSet, function (data, index) {
+  //     requestData(_url + '?type=' + data.type + '&nu=' + data.nu, {}, 'get')
+  //     .then(function (results) {
+  //       // console.log(results[1]);
+  //       var _tmpObj = results[1];
+  //       _tmpObj.index = index;
+  //       _tmpObj.type = data.type;
+  //       _tmpObj.nu = data.nu;
+  //       $scope.expressInfoArray.push(_tmpObj);
+  //     });
+  //   });
+  // }
+  /**
+   * [getAllExpressController 获取当前单据的所有物流信息]
+   * @param  {[type]} $scope [description]
+   * @return {[type]}        [description]
+   */
+
   function getAllExpressController ($scope, requestData) {
 
     var _kuaidiSet = $scope.dialogData.kuaidiSet;
@@ -5241,6 +5265,18 @@ define('project/controllers', ['project/init'], function() {
     $scope.expressInfoArray = [];
 
     angular.forEach(_kuaidiSet, function (data, index) {
+
+        //46 经销商平台，销售出库单，只有第三个快递有数据，点击查看物流信息时，切换快递，变成第一个快递有物流信息
+        //由于异步请求，返回数据，没有按照顺序返回导致数据有错位的bug
+      var _tmpObj ={};
+      _tmpObj.index = index;
+      _tmpObj.type = data.type;
+      _tmpObj.nu = data.nu;
+      //加载中,加载成功，加载失败
+      _tmpObj.status="加载中";
+      _tmpObj.msg="加载中";
+      $scope.expressInfoArray.push(_tmpObj);
+
       requestData(_url + '?type=' + data.type + '&nu=' + data.nu, {}, 'get')
       .then(function (results) {
         // console.log(results[1]);
@@ -5248,7 +5284,13 @@ define('project/controllers', ['project/init'], function() {
         _tmpObj.index = index;
         _tmpObj.type = data.type;
         _tmpObj.nu = data.nu;
-        $scope.expressInfoArray.push(_tmpObj);
+        _tmpObj.status="完成";
+        $scope.expressInfoArray[index]=_tmpObj;
+
+      }).catch(function (error) {
+        // alertError(error || '出错');
+        $scope.expressInfoArray[index].status="加载失败";
+        $scope.expressInfoArray[index].msg=error || '出错';
       });
     });
   }
