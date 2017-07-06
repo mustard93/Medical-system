577d985db79487649252eaf1e0c1f1f6a01b2430
venray <2469415004@qq.com>
2017-07-06 18:13:51 +0800

    资质预警

--- src/angular/modules/main/controllers.js
+++ src/angular/modules/main/controllers.js
@@ -191,9 +191,20 @@ define('main/controllers', ['main/init'], function () {
           }
         };
 
+
+        //计算某个列表中对象的某属性的和
+        $scope.countAttrVal=function (list,attr) {
+            var sum=0;
+            angular.forEach(list,function (item,index) {
+                sum+= (1*item[attr]);
+            });
+            return sum;
+        };
+
         $rootScope.hasAuthor = $scope.hasAuthor;
         $rootScope.hasAuthorOr = $scope.hasAuthorOr;
         $rootScope.hasAuthorAnd = $scope.hasAuthorAnd;
+        $rootScope.countAttrVal = $scope.countAttrVal;
 
 
 
