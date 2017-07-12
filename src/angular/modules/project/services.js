f61e65436d787eff7cf5a7556a4160ab8196e411
宋娟songjuan <1490590465@qq.com>
2017-07-12 13:53:39 +0800

    修改销退采退

--- src/angular/modules/project/services.js
+++ src/angular/modules/project/services.js
@@ -800,7 +800,7 @@ define('project/services', ['project/init'], function() {
                 bottomButton = {
                     "iconClass": "watch-detail-icon",
                     "showName": "查看详情",
-                    "ngShow": "tr.orderStatus=='处理中' || tr.orderStatus=='待审核' || tr.orderStatus=='已处理'|| tr.orderStatus=='已作废'",
+                    "ngShow": "tr.orderStatus=='处理中' || tr.orderStatus=='已处理'|| tr.orderStatus=='已作废'",
                     "aclass": "btn-link pd-m rect-s",
                     "ahref": "#/saleReturnOrder/get.html?id=" + showData.id
                 };
