b8f5f40899aced6bc002523a2e7b9be7ee3f19a7
宋娟songjuan <1490590465@qq.com>
2017-07-14 19:23:28 +0800

    修复bug 527

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -1347,6 +1347,28 @@ define('project/controllers', ['project/init'], function() {
 
     };
 
+    $scope.diffPurchaseNumber = function (orderMedicalList) {
+      if (orderMedicalList) {
+        // 用于放每一条判断数量后的结果
+        isDisabledNextStepList=[];
+        angular.forEach(orderMedicalList, function (data, index) {
+          // 选择的数量小于计划数量，显示提示信息
+          $scope.isShowPurchaseInfo = (data.planQuantity > data.quantity) ? true : false;
+          // ..
+          $scope.isDisabledNextStep = (data.quantity > data.planQuantity) ? true : false;
+          // 把每一条判断后的true或者是false放入数组中
+          isDisabledNextStepList.push($scope.isDisabledNextStep);
+        });
+        // 用some方法判断只要有一条为true，就阻止提交。相反，若全为false。就允许提交
+        if (isDisabledNextStepList.some(function(item){ return item == true;}))
+        {
+          return $scope.isDisabledNextStep=true;
+        }else{
+          return $scope.isDisabledNextStep=false;
+        }
+      }
+    };
+
     // 总价计算方法
     $scope.confirmOrderCalculaTotal = function (orderMedicalNos, orderBusinessType) {
       if (orderMedicalNos) {
@@ -2247,7 +2269,7 @@ define('project/controllers', ['project/init'], function() {
 
               $scope.isDisabledNextStep = (data.quantity== 0 || data.quantity > data.planQuantity ) ? true : false;
               // 把每一条判断后的true或者是false放入数组中
-              
+
               isDisabledNextStepList.push($scope.isDisabledNextStep);
             });
             // 用some方法判断只要有一条为true，就阻止提交。相反，若全为false。就允许提交
