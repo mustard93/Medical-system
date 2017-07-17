e2523b99a12f726c7279b78ca4f839e32691e81b
阿振sc <3494688@qq.com>
2017-07-17 21:05:01 +0800

    修复部分新建单据中对部门id的校验问题

--- src/angular/modules/main/directives.js
+++ src/angular/modules/main/directives.js
@@ -2332,6 +2332,7 @@ $attrs.callback:异步加载 成功后，回调执行代码行。作用域$scope
 
                         // chosenObj&&chosenObj.data("chosen").single_set_selected_text();
                         getData();
+
                     });
                   }
 
