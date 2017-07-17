46661ed2b0df57819bed8544410e864f4827cd59
yjjewg11 <41049630@qq.com>
2017-07-17 22:11:16 +0800

    开发规范按照模块拆分controllers控制器。参考API说明： http://localhost:3000/src/angularjs-api/index.html#/modules/controller.html

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -1,48 +1,9 @@
 /**
  * Created by hao on 15/11/5.
  */
-define('project/controllers', ['project/init'], function() {
+define('project/controllers', ['project/init','project/controllers-imTaobao'], function() {
 
 
-  /**
-   * [saleReturnMedicalItemController 新建销退单药品列表tr控制器]
-   * @param  {[type]} $scope [description]
-   * @return {[type]}        [description]
-   */
-  function imTaobaoCtr ($scope,requestData,alertError,$rootScope) {
-    //弹出与某人聊天.toUserId 聊天 人的userid
-    $scope.openIm = function (toUserId, toUserName) {
-      var imTaobaoUserInfo=  $rootScope.MyImTaobaoUserInfo;
-      if(!imTaobaoUserInfo){//没有帐号就获取帐号
-        imTaobaoUserInfo_getMy(function(){$scope.openIm(toUserId)});
-        return ;
-      }
-      if(!toUserId)toUserId="";
-      if(!toUserName)toUserName="";
-      var param="?uid="+imTaobaoUserInfo.userid+"&to="+toUserId+"&toUserName="+toUserName+"&appkey=23588140&pwd="+imTaobaoUserInfo.password+"&fullscreen";
-      window.open('../imtaobao/kit.html'+param, 'webcall', 'toolbar=no, status=no,scrollbars=0,resizable=0,menubar＝0,location=0,width=700,height=530');
-
-    };
-
-    //获取我的聊天帐号信息
-    function imTaobaoUserInfo_getMy(callback) {
-      var url='rest/authen/imTaobaoUserInfo/getMy.json';
-      requestData(url)
-        .then(function (results) {
-          var _data = results[0];
-          $rootScope.MyImTaobaoUserInfo= results[0];
-          if(callback){
-            callback();
-          }
-        })
-        .catch(function (error) {
-          alertError(error || '出错');
-        });
-    }//imTaobaoUserInfo_getMy
-
-
-
-  }//imTaobaoCtr
 
 
   /**
@@ -8201,7 +8162,6 @@ define('project/controllers', ['project/init'], function() {
   .controller('indexPageController', ['$scope', 'utils', indexPageController])
   .controller('getAllExpressController', ['$scope', 'requestData', getAllExpressController])
   .controller('saleOutstockOrderController', ['$scope', 'requestData', 'utils', saleOutstockOrderController])
-  .controller('imTaobaoCtr', ['$scope',"requestData",'alertError',"$rootScope", imTaobaoCtr])
   .controller('saleReturnMedicalItemController', ['$scope', saleReturnMedicalItemController])
   .controller('returnOrderAddController', ["$scope", "$rootScope", "modal","utils", "requestData", "alertError", returnOrderAddController])
   .controller('mainCtrlProject',  ["$scope","$rootScope","$http", "$location", "store","utils","modal","OPrinter","UICustomTable","bottomButtonList","saleOrderUtils","purchaseOrderUtils","requestPurchaseOrderUtils","queryItemCardButtonList","customMenuUtils", mainCtrlProject])
