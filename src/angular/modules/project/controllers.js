c7c055c812f8a4f422f3d2cc1414d3b2bc2b9427
tianwang <tianwang>
2017-07-17 20:06:47 +0800

    返回销退id

--- src/angular/modules/project/controllers.js
+++ src/angular/modules/project/controllers.js
@@ -7395,6 +7395,147 @@ define('project/controllers', ['project/init'], function() {
 
   }
 
+
+
+    function regionManageController ($scope, alertOk, alertError, alertWarn, requestData, utils) {
+
+        // 定义是否显示右侧编辑界面
+        $scope.showEditArea = false;
+
+        // 定义保存节点信息类型，默认为修改节点信息
+        $scope.modifyNodeInfo = true;
+
+        // 获取模块名，（商品分类模块、供应商分类模块、客户分类模块）
+        // $scope.$watch('moduleName', function (newVal, oldVal) {
+        //   if (newVal && newVal !== oldVal) {
+        //     // 定义保存url
+        //     var _saveUrl = 'rest/authen/' + $scope.moduleName + '/save.json';
+        //     // 定义删除请求地址
+        //     var _delUrl = 'rest/authen/' + $scope.moduleName + '/delete?id=';
+        //   }
+        // });
+
+        // console.log(_saveUrl + '==>' +_delUrl);
+
+        // 转换返回的JSON对象为JSON字符串
+        $scope.filterJSONDate = function (data) {
+            return JSON.stringify(data);
+        }
+
+        // 关闭新增主分类区域
+        $scope.cancelAddClass = function () {
+
+            $scope.showAddClass = $scope.showAddClass ? false :true;
+
+            // if ($scope.showAddClass) {
+            //   $scope.showAddClass = false;
+            // } else {
+            //   $scope.showAddClass = false;
+            // }
+        }
+
+        // 添加主分类
+        $scope.addMainClass = function (addMainClassObj) {
+            if (addMainClassObj) {
+                // 保存路径
+                var _saveUrl = 'rest/authen/' + $scope.moduleName + '/save.json';
+                // 发送请求保存数据
+                requestData(_saveUrl, addMainClassObj, 'POST', 'parameterBody')
+                    .then(function (resutls) {
+                        if (resutls[1].code === 200) {
+                            alertOk('操作成功');
+                            utils.refreshHref();
+                        }
+                    })
+                    .catch(function (error) {
+                        if (error) { alertWarn(error) }
+                    });
+            }
+        }
+
+        // 修改节点信息和保存子节点信息操作
+        $scope.saveNodeInfo = function (regionManage) {
+            // 保存路径
+            var _saveUrl = 'rest/authen/' + $scope.moduleName + '/save.json';
+
+            if ($scope.modifyNodeInfo) {     // 修改节点信息
+                requestData(_saveUrl, regionManage, 'POST', 'parameterBody')
+                    .then(function (results) {
+                        if (results[1].code === 200) {
+                            alertOk('操作成功');
+                            utils.refreshHref();
+                        } else {
+                            alertWarn(results[1].msg);
+                        }
+                    })
+                    .catch(function (error) {
+                        if (error) { alertWarn(error); }
+                    })
+            } else {      // 新增子节点
+                // 如果父节点id为空，则将当前节点id复制给父节点
+                if (!regionManage['parentId']) {
+                    regionManage['parentId'] = angular.copy(regionManage['id']);
+                }
+                // 将id置空，标识为新建节点
+                regionManage['id'] = null;
+
+                requestData(_saveUrl, regionManage, 'POST', 'parameterBody')
+                    .then(function (results) {
+                        if (results[1].code === 200) {
+                            alertOk('操作成功');
+                            utils.refreshHref();
+                        } else {
+                            alertWarn(results[1].msg);
+                        }
+                    })
+                    .catch(function (error) {
+                        if (error) { alertWarn(error); }
+                    });
+            }
+        }
+
+        // 删除类别
+        $scope.deleteThisClass = function () {
+            if ($scope.formData.regionManage.id) {
+                // 删除路径
+                _delUrl = 'rest/authen/' + $scope.moduleName + '/delete?id=' + $scope.formData.regionManage.id
+                requestData(_delUrl, {}, 'POST')
+                    .then(function (results) {
+                        if (results[1].code === 200) {
+                            // $scope._reloadData('rest/authen/medicalAttribute/query.json', 'scopeTreeData2')
+                            utils.refreshHref();
+                        } else {
+                            alertWarn(results[1].msg);
+                        }
+                    })
+                    .catch(function (error) {
+                        if (error) { alertWarn(error); }
+                    });
+            }
+        }
+
+        // 新增子类节点
+        $scope.addNewChildNode = function () {
+
+            console.log($scope.formData.regionManage.levelCode);
+
+            if ($scope.formData.regionManage.levelCode && $scope.formData.regionManage.showName) {
+                // 设置标识符
+                $scope.modifyNodeInfo = false;
+
+                if (!$scope.formData.regionManage.parentCode) {
+                    $scope.formData.regionManage.parentCode = '';
+                }
+
+                $scope.formData.regionManage.parentCode = angular.copy($scope.formData.regionManage.parentCode + $scope.formData.regionManage.levelCode);
+                $scope.formData.regionManage.parentId = $scope.formData.regionManage.id;
+                $scope.formData.regionManage.levelCode = null;
+                $scope.formData.regionManage.showName = null;
+            }
+        }
+
+    }
+
   /**
    * 借出单编辑Ctrl
    * @param $scope
@@ -8347,7 +8488,11 @@ define('project/controllers', ['project/init'], function() {
   .controller('returnOrderCtrl', ['$scope','modal', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', returnOrderCtrl])
   .controller('returnOrderChoiceDialogCtrl', ['$scope','modal', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', returnOrderChoiceDialogCtrl])
   .controller('returnOrderChoiceDialogSubCtrl', ['$scope','modal', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', returnOrderChoiceDialogSubCtrl])
-  .controller('choseBatchCtrl', ['$scope','modal', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', choseBatchCtrl]);
+  .controller('choseBatchCtrl', ['$scope','modal', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', choseBatchCtrl])
+    .controller('regionManageController', ['$scope','modal', 'watchFormChange', 'requestData', 'utils','alertError','alertWarn', regionManageController]);
+
+
+
 
 
 });
