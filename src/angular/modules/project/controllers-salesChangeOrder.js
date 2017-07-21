dba00f5fa8a83852e2f6ba9093baea4100ce00ff
venray <2469415004@qq.com>
2017-07-21 10:01:08 +0800

    销售换货

--- src/angular/modules/project/controllers-salesChangeOrder.js
+++ src/angular/modules/project/controllers-salesChangeOrder.js
@@ -15,14 +15,10 @@ define('project/controllers-salesChangeOrder', ['project/init'], function() {
 
   function  salesChangeOrderEditCtrl($scope, modal, watchFormChange, requestData, utils, alertError, alertWarn) {
 
-      
-      
-      
-      
-      //表单数据监控
-      $scope.watchFormChange = function(watchName){
-          watchFormChange(watchName,$scope);
-      };
+    //表单数据监控
+    $scope.watchFormChange = function(watchName){
+      watchFormChange(watchName,$scope);
+    };
 
     //校验计划归还输入数量   待还数量= 借出数量 - 已还数量
     $scope.checkQuantity=function(tr){
@@ -43,13 +39,18 @@ define('project/controllers-salesChangeOrder', ['project/init'], function() {
         return count;
     };
 
-    // 检查归还数量是否合法
+    // 检查批号与数量是否合法
     $scope.checkCanSubmit=function () {
         var flag=true;
 
+        if(!$scope.formData.orderMedicalNos){
+            return false;
+        }
+
         angular.forEach($scope.formData.orderMedicalNos,function (tr,index) {
-            //实际归还数量大于待还数量 或 实际待还数量小于1 ，认为数量不合法
-            if((tr.actualCount - tr.cumulativeReturnCount) < tr.quantity  || tr.quantity <1){
+
+            //换货数量 大于 已选择的批次总和  认为数量不合法
+            if((tr.stayCount > $scope.getAllBatchTotal(tr.stockBatchs,'quantity'))){
                 flag=false;
             }
         });
@@ -60,66 +61,84 @@ define('project/controllers-salesChangeOrder', ['project/init'], function() {
     // 回调  保存type:save-草稿,submit-提交订单。
     $scope.submitFormAfter = function() {
 
-        if($scope.submitForm_type == 'save'){
-            $scope.goTo('#/salesChangeOrder/edit.html?id='+$scope.formData.id);
-            return;
-        }
+    if($scope.submitForm_type == 'save'){
+        $scope.goTo('#/salesChangeOrder/edit.html?id='+$scope.formData.id);
+        return;
+    }
 
-        if ($scope.submitForm_type == 'submit') {
-            var _url='rest/authen/salesChangeOrder/startProcessInstance';
-            var data= {businessKey:$scope.formData.id};
-            requestData(_url,data, 'POST')
-                .then(function (results) {
-                    var _data = results[1];
-                    $scope.goTo('#/salesChangeOrder/get.html?id='+$scope.formData.id);
-                })
-                .catch(function (error) {
-                    alertError(error || '出错');
-                });
-        }
+    if ($scope.submitForm_type == 'submit') {
+        var _url='rest/authen/salesChangeOrder/startProcessInstance';
+        var data= {businessKey:$scope.formData.id};
+        requestData(_url,data, 'POST')
+            .then(function (results) {
+                var _data = results[1];
+                $scope.goTo('#/salesChangeOrder/get.html?id='+$scope.formData.id);
+            })
+            .catch(function (error) {
+                alertError(error || '出错');
+            });
+    }
 
     };
 
     // 保存type:save-草稿,submit-提交订单。
     $scope.submitForm = function(fromId, type) {
 
-        $scope.submitForm_type = type;
+    $scope.submitForm_type = type;
 
-        // 如果点击提交无效，再次修改提交对象中的值，则在保存点击时将后端验证标识设置为false
-        if ($scope.submitForm_type === 'save') {
-            $scope.formData.validFlag = false;
-        }
+    // 如果点击提交无效，再次修改提交对象中的值，则在保存点击时将后端验证标识设置为false
+    if ($scope.submitForm_type === 'save') {
+        $scope.formData.validFlag = false;
+    }
 
-        if ($scope.submitForm_type == 'submit') {
-            $scope.formData.validFlag = true;
-        }
+    if ($scope.submitForm_type == 'submit') {
+        $scope.formData.validFlag = true;
+    }
 
-        $('#' + fromId).trigger('submit');
+    $('#' + fromId).trigger('submit');
     };
 
-   // 重置 借出单 信息
-   $scope.resetLendOrderInfo=function () {
+    // 重置 借出单 信息
+    $scope.resetLendOrderInfo=function () {
 
-       if($scope.formData.orderMedicalNos.length<1){
-           $scope.formData.relId="";//关联原订单ID
-           $scope.formData.relOrderNo="";//关联原订单号
-           $scope.formData.relOrderCode="";//关联原订编号
-
-
-           //发货方信息   收货方信息
-           $scope.formData.customerContacts=null;
-           $scope.formData.invoicesContacts=null;
+    if($scope.formData.orderMedicalNos.length<1){
+       $scope.formData.relId="";//关联原订单ID
+       $scope.formData.relOrderNo="";//关联原订单号
+       $scope.formData.relOrderCode="";//关联原订编号
 
 
-       }
-   }
+       //发货方信息   收货方信息
+       $scope.formData.customerContacts=null;
+       $scope.formData.invoicesContacts=null;
 
-     //获取所有批次的和
-     $scope.getAllBatchTotal=function (list,attr) {
+
+    }
+    }
+
+
+    //获取所有批次的和
+    $scope.getAllBatchTotal=function (list,attr) {
         return utils.countAttrVal(list,attr);
-     }
+    };
 
-   
+
+    //更新药械的换货数量
+    $scope.updateMedcalInfo=function (index,item) {
+
+     $scope.formData.orderMedicalNos[index].stayCount= item.stayCount;
+
+     //   判断当前药械的 “换货数量” 是否 小于 已选择批次总数； 小于: 清空批次信息
+     if($scope.formData.orderMedicalNos[index].stayCount < $scope.getAllBatchTotal($scope.formData.orderMedicalNos[index].stockBatchs,'quantity')){
+         $scope.formData.orderMedicalNos[index].stockBatchs=[];
+     }
+    };
+
+    //删除列表中的药械
+    $scope.deleteMedcal=function (index) {
+     $scope.formData.receiveOrderMedicalNos.splice(index,1);
+     $scope.formData.orderMedicalNos.splice(index,1);
+    };
+
   }
 
   //销售换货单弹窗 Ctrl
@@ -218,70 +237,44 @@ define('project/controllers-salesChangeOrder', ['project/init'], function() {
         };
 
         //添加商品到列表
-        $scope.addOrderDataToList=function (orderInfo) {
+        $scope.addOrderDataToList=function (invoicesOrderId,orderInfo) {
 
             console.log("orderInfo>>>>>>>>>>>>>>>",orderInfo);
 
-            // //如果存在就判断是否相等  不相等就返回
-            // if(invoicesOrderId){
-            //
-            //     if(invoicesOrderId != orderInfo.id){
-            //         alertWarn("只能选择同一借出单药械");
-            //         return;
-            //     }
-            //
-            // }else{
-            //     //如果不存在就设置
+            //如果存在就判断是否相等  不相等就返回
+            if(invoicesOrderId && invoicesOrderId != orderInfo.id){
 
+               // if(invoicesOrderId != orderInfo.id){
+                    alertWarn("只能选择同一换货单药械");
+                    return;
+               // }
 
+            }else{
+                //如果不存在就设置
+                $scope.formData.relId=orderInfo.id;
+                $scope.formData.relOrderNo= orderInfo.orderNo;
 
+                $scope.formData.relOrderCode=orderInfo.orderCode; //发货单编号
+                $scope.formData.customerName=  orderInfo.customerName; //设置客户名
+                $scope.formData.customerId=orderInfo.customerId;//设置客户ID
 
-            $scope.formData.relId=orderInfo.id;
-            $scope.formData.relOrderNo= orderInfo.orderNo;
-
-
-            $scope.formData.relOrderCode=orderInfo.orderCode; //发货单编号
-            $scope.formData.customerName=  orderInfo.customerName; //设置客户名
-            $scope.formData.customerId=orderInfo.customerId;//设置客户ID
-
-
-            // }
+            }
 
             //添加商品
             var hasOrderMedicalNos = $scope.formData.receiveOrderMedicalNos;
 
-            //添加的商品设置的计划归还数量为null, 在页面进行计算；
-            // angular.forEach($scope.selectedBatchs2,function (item,index) {
-            //     item.quantity = null;
-            //     $scope.selectedBatchs2[index]= item;
-            // });
 
             var resultArr = $scope._compareArray(hasOrderMedicalNos,$scope.selectedBatchs2,'id','id');
             $scope.formData.receiveOrderMedicalNos = hasOrderMedicalNos.concat(resultArr);
 
-
-            angular.forEach($scope.selectedBatchs2,function (item,index) {
-
-
-                //判断添加的药械是否存在列表， 如果存在，更新“换货数量”，否者 就添加
-                if( $scope.itemInArray(item.id,$scope.formData.orderMedicalNos,'id')){
-                    //如果存在，更新“换货数量”
-
-
-                }else{
-                    $scope.formData.orderMedicalNos.push(item);
-                }
-
+            //添加到重新发货列表
+            angular.forEach(resultArr,function (item,index) {
+                item.stayCount= utils.countAttrVal(item.stockBatchs,'quantity') || 0;
+                item.stockBatchs=[];
+                $scope.formData.orderMedicalNos.push(item);
             });
 
-
-
         };
-
-
-
-
-
 
         /**
          * 判断item 是否存在
