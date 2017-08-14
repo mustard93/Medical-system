99434a5a41bc6b5b730575cc973449e0bebfef57
lixiangjiang <lixiangjiang@163.com>
2017-08-14 15:14:44 +0800

    1127 修复已完成购需单，生成销售单部分，有一个展开收起功能，第一次要点2次才生效，请确认

--- src/angular/modules/project/directives.js
+++ src/angular/modules/project/directives.js
@@ -734,16 +734,6 @@ function togglePanel () {
     restrict: 'A',
     link: function ($scope, element, $attrs) {
 
-      // 扩展用于盘点模块：按货位盘点，已选的货位，打开侧边框后会打开相应货位的panel
-      if ($attrs.toggleChecked) {
-
-        var toggleChecked=$attrs.toggleChecked;
-
-        if (toggleChecked=="true") {
-            $(element).parents(".panel").children(".panel-body").slideDown(200);
-        }
-      }
-
       $(element).on('click', function (e) {
         e.stopPropagation();
 
@@ -761,6 +751,18 @@ function togglePanel () {
         }
 
       });
+
+      // 扩展用于盘点模块：按货位盘点，已选的货位，打开侧边框后会打开相应货位的panel
+      if ($attrs.toggleChecked) {
+
+        var toggleChecked=$attrs.toggleChecked;
+
+        if (toggleChecked=="true") {
+            // $(element).parents(".panel").children(".panel-body").slideDown(200);
+            $(element).click();
+
+        }
+      }
 
     }
   };
