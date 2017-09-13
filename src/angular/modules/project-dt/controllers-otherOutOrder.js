f441572ab562aa1288a412a7d21d22a93b30f782
lixiangjiang <lixiangjiang@163.com>
2017-09-13 17:59:00 +0800

    其他业务单

--- src/angular/modules/project-dt/controllers-otherOutOrder.js
+++ src/angular/modules/project-dt/controllers-otherOutOrder.js
@@ -680,9 +680,9 @@ define("project-dt/controllers-otherOutOrder", ['project-dt/init'], function(){
       $scope.amountCalcuConfirmOrder = new AmountCalculationService();
     }
 
-    // 根据订单类型返回数量值
+    // 根据订单类型返回数量值-->返回所有批次数量的和
     $scope.returnQuantityByOrderType = function (typeName, medicalObj) {
-      if (typeof typeName === 'string' && typeName.indexOf('普通') > -1) {    //  普通销售类型，返回所有批次数量的和
+      // if (typeof typeName === 'string' && typeName.indexOf('普通') > -1) {    //  普通销售类型，返回所有批次数量的和
         if (angular.isObject(medicalObj) && medicalObj.stockBatchs.length) {
           var _quantity = 0;
           angular.forEach(medicalObj.stockBatchs, function (item, index) {
@@ -691,9 +691,9 @@ define("project-dt/controllers-otherOutOrder", ['project-dt/init'], function(){
 
           return _quantity;
         }
-      } else if (typeof typeName === 'string' && typeName.indexOf('直发') > -1) {   // 直发销售类型，直接返回购需数量
-        return medicalObj.quantity;
-      }
+      // } else if (typeof typeName === 'string' && typeName.indexOf('直发') > -1) {   // 直发销售类型，直接返回购需数量
+        // return medicalObj.quantity;
+      // }
     }
 
     // ...
